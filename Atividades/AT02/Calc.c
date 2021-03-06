3902d471df229bf45571ecfdcd478e95672f837b
caraujo-geo <caraujo.geo@gmail.com>
2021-03-06 16:37:07 -0300

    Atividades
    Atividade 02

--- Atividades/AT02/Calc.c
+++ Atividades/AT02/Calc.c
@@ -7,14 +7,34 @@ int main(){
 	
 	
 	while(operacao != 0){
-
-	printf ("Insira o primeiro numero:\n");
-	scanf ("%d", &num1);
-	printf ("Insira o segundo numero:\n");
-	scanf ("%d", &num2);
-
-	result = num1 + num2;
- 	printf("O resultado da operacao eh: %.2f", result);
+    
+    	printf("Informe o tipo de operação que deseja realizar:\n0 - Sair  \t1 - Somar \t2 - Subtrair \t3 - Multiplicar \t4 - Dividir\n");
+    	scanf("%d", &operacao);
+		printf ("Insira o primeiro numero:\n");
+		scanf ("%d", &num1);
+		printf ("Insira o segundo numero:\n");
+		scanf ("%d", &num2);
+			switch (operacao){
+				case 1:
+				result = num1 + num2;
+ 				printf("O resultado da operacao eh: %.2f\n", result);
+ 				break;
+ 				case 2:
+ 				result = num1 - num2;
+ 				printf("O resultado da operacao eh: %.2f\n", result);
+ 				break;
+ 				case 3:
+ 				result = num1 * num2;
+ 				printf("O resultado da operacao eh: %.2f\n", result);
+ 				break;
+ 				case 4:
+ 				result = num1 / num2;
+ 				printf("O resultado da operacao eh: %.2f\n", result);
+ 				break;
+ 				default:
+ 				printf("Opção invalida\n");		
+		}
+	if (operacao==0)break;
  	}
  	return 0;
 
